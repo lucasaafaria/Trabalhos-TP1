36b9740e1dd38aa8d805bb2e7644462dae3d7694
Lucas Faria <31375155+lucasaafaria@users.noreply.github.com>
2018-11-23 01:50:47 -0200

    adicionados comentários comandos.h

--- Trabalho2/comandos.h
+++ Trabalho2/comandos.h
@@ -1,32 +1,63 @@
 #ifndef COMANDOS_H_
 #define COMANDOS_H_
 
+// Relação de bibliotecas utilizadas na declaração das classes de comandos
+
 #include "../Trabalho1/dominios.h"
 #include "../Trabalho1/entidades.h"
 #include "interfaces.h"
 
+// As classes de comando são responsáveis por solicitar serviços específicos do módulo de
+// Serviços de Usuário.
+
+// ---------------------------------------------------------------------------------------- //
+// Classe Abstrata Pura Cmd:
+
 class Cmd {
 public:
+
+  // Declaração de métodos virtuais puros
+
 	virtual void executar(ISerUsuario * servidor, Identificador * id, bool * autenticado) = 0;
 	virtual ~Cmd(){}
 };
 
+// ---------------------------------------------------------------------------------------- //
+// Classe CmdCadastrar, herdada de Cmd:
+
 class CmdCadastrar:public Cmd {
 public:
+
+  // Métodos da classe
+
   void executar(ISerUsuario * servidor, Identificador * id, bool * autenticado);
   ~CmdCadastrar(){}
 };
 
+// ---------------------------------------------------------------------------------------- //
+// Classe CmdEditar, herdada de Cmd:
+
 class CmdEditar:public Cmd {
 public:
+  
+  // Métodos da classe
+
   void executar(ISerUsuario * servidor, Identificador * id, bool * autenticado);
   ~CmdEditar(){}
 };
 
+// ---------------------------------------------------------------------------------------- //
+// Classe CmdDescadastrar, herdada de Cmd:
+
 class CmdDescadastrar:public Cmd {
-public: 
+public:
+
+  // Métodos da classe
+
   void executar(ISerUsuario * servidor, Identificador * id, bool * autenticado);
   ~CmdDescadastrar(){}
 };
+
+// ---------------------------------------------------------------------------------------- //
 
 #endif // COMANDOS_H_
